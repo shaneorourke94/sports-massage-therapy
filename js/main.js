df3ec12f0aff66f07a33cde31457335e76823e2e
shane.orourke <shaneorourke88@hotmail.com>
2017-09-06 15:31:18 +0100

    Adding javascript and visitor counter

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+var txtFile = "data/visitors.txt";
+var reader = new FileReader();
+reader.readAsText(txtFile, 1);
+
+function go_to_contact_info() {
+    location.href = "#contact";
+}
+
+function myMap() {
+    var myCenter = new google.maps.LatLng(54.6348, -8.4547);
+    var mapProp = {
+        center: myCenter,
+        zoom: 15,
+        scrollwheel: false,
+        draggable: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var marker = new google.maps.Marker({
+        position: myCenter
+    });
+    marker.setMap(map);
+}
+
+$(document).ready(function () {
+    $(".navbar a, footer a[href='#home'], .btn").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+})
+
+$(window).scroll(function () {
+    $(".slideanim").each(function () {
+        var pos = $(this).offset().top;
+
+        var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+            $(this).addClass("slide");
+        }
+    });
+});
